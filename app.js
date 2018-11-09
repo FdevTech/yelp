2d697e659989e2820f634e8f00988a3a7cb72b98
FdevTech <fares18101991@gmail.com>
2018-11-09 10:30:50 +0100

    adding the function wwhich calls the landing page

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express=require("express");
 var app=express();
+app.get("/",function(req,res){
+res.send("this will be our landing page soon!")
+});
 app.listen(3000,function()
 {
 console.log("the yelp project has started");
